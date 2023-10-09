0611c68d7f1a3e4e76c3777786cd5865bece2daf
luke.yu <yangyi.yu.luke@gmail.com>
2023-10-10 03:38:45 +1100

    update database version configuration

--- bookgallery-web/src/pages/BookUpdatePage/BookUpdatePage.js
+++ bookgallery-web/src/pages/BookUpdatePage/BookUpdatePage.js
@@ -1,6 +1,6 @@
 export const BookUpdatePage = () => {
     !window.localStorage.getItem("bookId") && window.localStorage.setItem("bookId", 1)
-    console.log('frontend is running in docker not');
+    console.log('frontend is running in docker');
     console.log(`The current book id is ${window.localStorage.getItem("bookId")}`);
     return <h1>BookUpdatePage</h1>
 }
